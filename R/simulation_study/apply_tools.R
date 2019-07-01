29a09dfd175e87036e98b26a451b7dd2ac93d283
Alemu Takele Assefa <alemutak@hotmail.com>
2019-07-02 00:43:22 +0200

    Update apply_tools.R

--- R/simulation_study/apply_tools.R
+++ R/simulation_study/apply_tools.R
@@ -1,139 +1,140 @@
-
-source(".../tools_wrap_functions.R")
+source("tools_wrap_functions.R")
 
 # ------------ SPsimSeq simulated data analysis A -------------------------------
-sim.data.sp <- readRDS(".../sp.sim.dataA.rds")
+sim.data.sp <- readRDS("Simulation_study/SPsimA/sp.sim.dataA.rds")
 itr <- length(sim.data.sp)
 
 #PIMseq
-runPIMSeq1_A <- lapply(1:itr, function(k){
+resPIMSeq <- lapply(1:itr, function(k){
   sim.data <- sim.data.sp[[k]]
   # keep  <- rowSums(counts(sim.data)>1)>5
-  # sim.data <- sim.data[which(keep), ]
-  #plot(colSums(counts(sim.data)), colData(sim.data)$simLibeSize)
-  sim.dat$logLS <- log(colData(sim.dat)$ExpLibSize)
-  res <- run_PIMSeq(SCEdata = sim.data, condition.name = "Group",
-                     covariates = c("logLS"), expression.unit = "count",  prop.zero = 0.99)  
-  df <- merge(res$df, rowData(sim.data), by.x="ID", by.y="GeneID") 
-  res$all.res[, -1] <- sapply(res$all.res[,-1], function(x) as.numeric(as.character(x)))
-  df <- merge(df, res$all.res, by="ID")
+  # sim.data <- sim.data[which(keep), ] 
+  sim.data$logLS <- log(colData(sim.data)$sim.Lib.Size)
+  res <- run_PIMseq(SCEdata = sim.data, condition.name = "Group",
+                    covariates = c("logLS"), expression.unit = "counts",  prop.zero = 0.99)  
+  df <- merge(res$df, rowData(sim.data), by.x="ID", by.y="row.names") 
+  #res$all.res[, -1] <- sapply(res$all.res[,-1], function(x) as.numeric(as.character(x)))
+  #df <- merge(df, res$all.res, by="ID")
   
-  df2 <- res$res.augmented 
-  df2 <- merge(df2, rowData(sim.data), by.x="ID", by.y="GeneID") 
+  df2 <- res$res$augmented.MP
+  df2 <- merge(df2, rowData(sim.data), by.x="ID", by.y="row.names") 
   
   list(df=df, timing =res$timing, res.augmented= df2)
 })
-saveRDS(runPIMSeq1_A, ".../SPsimA/runPIMSeq1.rds")
+saveRDS(resPIMSeq, "Simulation_study/SPsimA/resPIMSeq.rds")
 
 #MAST
-runMAST_A <- lapply(1:itr, function(k){
+resMAST <- lapply(1:itr, function(k){
   res <- run_MAST(SCEdata = sim.data.sp[[k]], condition.name = "Group",
-                     expression.unit = "CPM", covariates = NULL, prop.zero = 0.99)  
+                  expression.unit = "CPM", covariates = NULL, prop.zero = 0.99)  
   
   res$df$ID <- rownames(res$df) 
-  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="GeneID") 
+  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="row.names") 
   
   list(df=df)
 })
-saveRDS(runMAST_A, ".../SPsimA/runMAST.rds")
+saveRDS(resMAST, "Simulation_study/SPsimA/resMAST.rds")
 
 
 #SAMSeq
-runSAMseq_A <- lapply(1:itr, function(k){
+resSAMseq <- lapply(1:itr, function(k){
   res <- run_SAMseq(SCEdata = sim.data.sp[[k]], condition.name = "Group",  
-                                  covariates = NULL, expression.unit = "count", prop.zero = 0.99)
+                    covariates = NULL, expression.unit = "count", prop.zero = 0.99)
   res$df$ID <- rownames(res$df) 
-  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="GeneID") 
-
+  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="row.names") 
+  
   list(df=df)
 })
-saveRDS(runSAMseq_A, ".../SPsimA/runSAMseq.rds")
+saveRDS(resSAMseq, "Simulation_study/SPsimA/resSAMseq.rds")
 
 
 #edgeR+Zinger
-result.edgeR_Zinger <- lapply(1:itr, function(k){
+res.edgeR_Zinger <- lapply(1:itr, function(k){
   res <- run_edgeR_Zinger(SCEdata = sim.data.sp[[k]], condition.name = "Group", 
                           covariates = NULL, expression.unit = "count", prop.zero = 0.99)
   res$df$ID <- rownames(res$df) 
-  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="GeneID") 
-
+  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="row.names") 
+  
   list(df=df)
 })
-saveRDS(result.edgeR_Zinger, ".../SPsimA/runZinger_EdgeR.rds")
+saveRDS(res.edgeR_Zinger, "Simulation_study/SPsimA/resZinger_EdgeR.rds")
 
 #DESeq2+Zinger
-result.DESeq2_Zinger <- lapply(1:itr, function(k){
+res.DESeq2_Zinger <- lapply(1:itr, function(k){
   res <- run_DESeq2_Zinger(SCEdata = sim.data.sp[[k]], condition.name = "Group",
                            covariates = NULL, expression.unit = "count", 
                            prop.zero = 0.99)
   res$df <- as.data.frame(res$df)
   res$df$ID <- rownames(res$df) 
-  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="GeneID") 
+  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="row.names") 
   
   list(df=df)
 })
 
-saveRDS(result.DESeq2_Zinger, ".../SPsimA/runZinger_DESeq2.rds")
+saveRDS(res.DESeq2_Zinger, "Simulation_study/SPsimA/resZinger_DESeq2.rds")
 
 
 
 
-  
+
 # ------------ Splat simulated data analysis  A -------------------------------
-sim.data.nb <- readRDS(".../nb.sim.dataA.rds")
+sim.data.nb <- readRDS("Simulation_study/NBsimA/nb.sim.dataA.rds")
 itr <- length(sim.data.nb)
-
+ 
 #PIMseq
-runPIMSeq1_A <- lapply(1:itr, function(k){
-  sim.dat <- sim.data.nb[[k]]
-  sim.dat$logLS <- log(colData(sim.dat)$ExpLibSize)
-  res <- run_PIMseq(SCEdata = sim.dat, condition.name = "Group", 
-                    covariates = c("logLS"), prop.zero = 0.99)  
-  df <- merge(res$df, rowData(sim.dat), by.x="ID", by.y="Gene")  
+resPIMSeq <- lapply(1:itr, function(k){
+  sim.data <- sim.data.nb[[k]]
+  sim.data$logLS <- log(colData(sim.data)$ExpLibSize)
+  res <- run_PIMseq(SCEdata = sim.data, condition.name = "Group",
+                    covariates = c("logLS"), expression.unit = "counts",  prop.zero = 0.99)  
+  df <- merge(res$df, rowData(sim.data), by.x="ID", by.y="Gene") 
+  
+  
   df2 <- res$res$augmented.MP
-  df2 <- merge(df2, rowData(sim.dat), by.x="ID", by.y="Gene") 
+  df2 <- merge(df2, rowData(sim.data), by.x="ID", by.y="row.names") 
   
   list(df=df, timing =res$timing, res.augmented= df2)
 })
-saveRDS(runPIMSeq1_A, ".../NBsimA/runPIMSeq1.rds")
+saveRDS(resPIMSeq, "Simulation_study/NBsimA/resPIMSeq.rds")
+
 
 #SAMSeq
-runSAMseq_A <- lapply(1:itr, function(k){
+resSAMseq <- lapply(1:itr, function(k){
   res <- run_SAMseq(SCEdata = sim.data.nb[[k]], condition.name = "Group",  
-                                  covariates = NULL, expression.unit = "count", prop.zero = 0.95)
+                    covariates = NULL, expression.unit = "count", prop.zero = 0.99)
   res$df$ID <- rownames(res$df) 
   df <- merge(res$df, rowData(sim.data.nb[[k]]), by.x="ID", by.y="Gene") 
-
+  
   list(df=df)
 })
-saveRDS(runSAMseq_A, ".../NBsimA/runSAMseq.rds")
+saveRDS(resSAMseq, "Simulation_study/NBsimA/resSAMseq.rds")
 
 #MAST
-runMAST_A <- lapply(1:itr, function(k){
+resMAST <- lapply(1:itr, function(k){
   res <- run_MAST(SCEdata = sim.data.nb[[k]], condition.name = "Group",
-                     expression.unit = "CPM", covariates = NULL, prop.zero = 0.99)  
+                  expression.unit = "CPM", covariates = NULL, prop.zero = 0.99)  
   
   res$df$ID <- rownames(res$df) 
   df <- merge(res$df, rowData(sim.data.nb[[k]]), by.x="ID", by.y="Gene") 
   
   list(df=df)
 })
-saveRDS(runMAST_A, ".../NBsimA/runMAST.rds")
+saveRDS(resMAST, "Simulation_study/NBsimA/resMAST.rds")
 
 
 #edgeR+Zinger
-result.edgeR_Zinger <- lapply(1:itr, function(k){
+res.edgeR_Zinger <- lapply(1:itr, function(k){
   res <- run_edgeR_Zinger(SCEdata = sim.data.nb[[k]], condition.name = "Group", 
-                                        covariates = NULL, expression.unit = "count", prop.zero = 0.99)
+                          covariates = NULL, expression.unit = "count", prop.zero = 0.99)
   res$df$ID <- rownames(res$df) 
   df <- merge(res$df, rowData(sim.data.nb[[k]]), by.x="ID", by.y="Gene") 
-
+  
   list(df=df)
 })
-saveRDS(result.edgeR_Zinger, ".../NBsimA/runZinger_EdgeR.rds")
+saveRDS(res.edgeR_Zinger, "Simulation_study/NBsimA/resZinger_EdgeR.rds")
 
 #DESeq2+Zinger
-result.DESeq2_Zinger <- lapply(1:itr, function(k){
+res.DESeq2_Zinger <- lapply(1:itr, function(k){
   res <- run_DESeq2_Zinger(SCEdata = sim.data.nb[[k]], condition.name = "Group",
                            expression.unit = "count", prop.zero = 0.99)
   res$df <- as.data.frame(res$df)
@@ -143,144 +144,146 @@ result.DESeq2_Zinger <- lapply(1:itr, function(k){
   list(df=df)
 })
 
-saveRDS(result.DESeq2_Zinger, ".../NBsimA/runZinger_DESeq2.rds")
+saveRDS(res.DESeq2_Zinger, "Simulation_study/NBsimA/resZinger_DESeq2.rds")
 
-  
-  
-  
-  # ------------ SPsimSeq simulated data analysis A -------------------------------
-sim.data.sp <- readRDS(".../sp.sim.dataB.rds")
+
+
+
+# ------------ SPsimSeq simulated data analysis B -------------------------------
+sim.data.sp <- readRDS("Simulation_study/SPsimB/sp.sim.dataB.rds")
 itr <- length(sim.data.sp)
-
+ 
 #PIMseq
-runPIMSeq1_A <- lapply(1:itr, function(k){
+resPIMSeq <- lapply(1:itr, function(k){
   sim.data <- sim.data.sp[[k]]
   # keep  <- rowSums(counts(sim.data)>1)>5
-  # sim.data <- sim.data[which(keep), ]
-  #plot(colSums(counts(sim.data)), colData(sim.data)$simLibeSize)
-  sim.dat$logLS <- log(colData(sim.dat)$ExpLibSize)
-  res <- run_PIMSeq(SCEdata = sim.data, condition.name = "Group",
-                     covariates = c("logLS"), expression.unit = "count",  prop.zero = 0.99)  
-  df <- merge(res$df, rowData(sim.data), by.x="ID", by.y="GeneID") 
-  res$all.res[, -1] <- sapply(res$all.res[,-1], function(x) as.numeric(as.character(x)))
-  df <- merge(df, res$all.res, by="ID")
+  # sim.data <- sim.data[which(keep), ] 
+  sim.data$logLS <- log(colData(sim.data)$sim.Lib.Size)
+  res <- run_PIMseq(SCEdata = sim.data, condition.name = "Group",
+                    covariates = c("logLS"), expression.unit = "counts",  prop.zero = 0.99)  
+  df <- merge(res$df, rowData(sim.data), by.x="ID", by.y="row.names") 
+  #res$all.res[, -1] <- sapply(res$all.res[,-1], function(x) as.numeric(as.character(x)))
+  #df <- merge(df, res$all.res, by="ID")
   
-  df2 <- res$res.augmented 
-  df2 <- merge(df2, rowData(sim.data), by.x="ID", by.y="GeneID") 
+  df2 <- res$res$augmented.MP
+  df2 <- merge(df2, rowData(sim.data), by.x="ID", by.y="row.names") 
   
   list(df=df, timing =res$timing, res.augmented= df2)
 })
-saveRDS(runPIMSeq1_A, ".../SPsimB/runPIMSeq.rds")
+saveRDS(resPIMSeq, "Simulation_study/SPsimB/resPIMSeq.rds")
 
 #MAST
-runMAST_A <- lapply(1:itr, function(k){
+resMAST <- lapply(1:itr, function(k){
   res <- run_MAST(SCEdata = sim.data.sp[[k]], condition.name = "Group",
-                     expression.unit = "CPM", covariates = NULL, prop.zero = 0.99)  
+                  expression.unit = "CPM", covariates = NULL, prop.zero = 0.99)  
   
   res$df$ID <- rownames(res$df) 
-  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="GeneID") 
+  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="row.names") 
   
   list(df=df)
 })
-saveRDS(runMAST_A, ".../SPsimB/runMAST.rds")
+saveRDS(resMAST, "Simulation_study/SPsimB/resMAST.rds")
 
-
-#SAMSeq
-runSAMseq_A <- lapply(1:itr, function(k){
-  res <- run_SAMseq(SCEdata = sim.data.sp[[k]], condition.name = "Group",  
-                                  covariates = NULL, expression.unit = "count", prop.zero = 0.99)
-  res$df$ID <- rownames(res$df) 
-  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="GeneID") 
-
-  list(df=df)
-})
-saveRDS(runSAMseq_A, ".../SPsimB/runSAMseq.rds")
 
 
 #edgeR+Zinger
-result.edgeR_Zinger <- lapply(1:itr, function(k){
+res.edgeR_Zinger <- lapply(1:itr, function(k){
   res <- run_edgeR_Zinger(SCEdata = sim.data.sp[[k]], condition.name = "Group", 
                           covariates = NULL, expression.unit = "count", prop.zero = 0.99)
   res$df$ID <- rownames(res$df) 
-  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="GeneID") 
-
+  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="row.names") 
+  
   list(df=df)
 })
-saveRDS(result.edgeR_Zinger, ".../SPsimA/runZinger_EdgeR.rds")
+saveRDS(res.edgeR_Zinger, "Simulation_study/SPsimB/resZinger_EdgeR.rds")
 
 #DESeq2+Zinger
-result.DESeq2_Zinger <- lapply(1:itr, function(k){
+res.DESeq2_Zinger <- lapply(1:itr, function(k){
   res <- run_DESeq2_Zinger(SCEdata = sim.data.sp[[k]], condition.name = "Group",
                            covariates = NULL, expression.unit = "count", 
                            prop.zero = 0.99)
   res$df <- as.data.frame(res$df)
   res$df$ID <- rownames(res$df) 
-  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="GeneID") 
+  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="row.names") 
   
   list(df=df)
 })
 
-saveRDS(result.DESeq2_Zinger, ".../SPsimB/runZinger_DESeq2.rds")
+saveRDS(res.DESeq2_Zinger, "Simulation_study/SPsimB/resZinger_DESeq2.rds")
 
-
-
-
+#SAMSeq
+resSAMseq <- lapply(1:itr, function(k){
+  res <- run_SAMseq(SCEdata = sim.data.sp[[k]], condition.name = "Group",  
+                    covariates = NULL, expression.unit = "count", prop.zero = 0.99)
+  res$df$ID <- rownames(res$df) 
+  df <- merge(res$df, rowData(sim.data.sp[[k]]), by.x="ID", by.y="row.names") 
   
+  list(df=df)
+})
+saveRDS(resSAMseq, "Simulation_study/SPsimB/resSAMseq.rds")
+
+
+
+
+
 # ------------ Splat simulated data analysis  B -------------------------------
-sim.data.nb <- readRDS(".../nb.sim.dataB.rds")
+sim.data.nb <- readRDS("Simulation_study/NBsimB/nb.sim.dataB.rds")
 itr <- length(sim.data.nb)
 
 #PIMseq
-runPIMSeq1_A <- lapply(1:itr, function(k){
-  sim.dat <- sim.data.nb[[k]]
-  sim.dat$logLS <- log(colData(sim.dat)$ExpLibSize)
-  res <- run_PIMseq(SCEdata = sim.dat, condition.name = "Group", 
-                    covariates = c("logLS"), prop.zero = 0.99)  
-  df <- merge(res$df, rowData(sim.dat), by.x="ID", by.y="Gene")  
+resPIMSeq <- lapply(1:itr, function(k){
+  sim.data <- sim.data.nb[[k]]
+  sim.data$logLS <- log(colData(sim.data)$ExpLibSize)
+  res <- run_PIMseq(SCEdata = sim.data, condition.name = "Group",
+                    covariates = c("logLS"), expression.unit = "counts",  prop.zero = 0.99)  
+  df <- merge(res$df, rowData(sim.data), by.x="ID", by.y="Gene") 
+  
+  
   df2 <- res$res$augmented.MP
-  df2 <- merge(df2, rowData(sim.dat), by.x="ID", by.y="Gene") 
+  df2 <- merge(df2, rowData(sim.data), by.x="ID", by.y="row.names") 
   
   list(df=df, timing =res$timing, res.augmented= df2)
 })
-saveRDS(runPIMSeq1_A, ".../NBsimB/runPIMSeq.rds")
+saveRDS(resPIMSeq, "Simulation_study/NBsimB/resPIMSeq.rds")
+
 
 #SAMSeq
-runSAMseq_A <- lapply(1:itr, function(k){
+resSAMseq <- lapply(1:itr, function(k){
   res <- run_SAMseq(SCEdata = sim.data.nb[[k]], condition.name = "Group",  
-                                  covariates = NULL, expression.unit = "count", prop.zero = 0.95)
+                    covariates = NULL, expression.unit = "count", prop.zero = 0.99)
   res$df$ID <- rownames(res$df) 
   df <- merge(res$df, rowData(sim.data.nb[[k]]), by.x="ID", by.y="Gene") 
-
+  
   list(df=df)
 })
-saveRDS(runSAMseq_A, ".../NBsimB/runSAMseq.rds")
+saveRDS(resSAMseq, "Simulation_study/NBsimB/resSAMseq.rds")
 
 #MAST
-runMAST_A <- lapply(1:itr, function(k){
+resMAST <- lapply(1:itr, function(k){
   res <- run_MAST(SCEdata = sim.data.nb[[k]], condition.name = "Group",
-                     expression.unit = "CPM", covariates = NULL, prop.zero = 0.99)  
+                  expression.unit = "CPM", covariates = NULL, prop.zero = 0.99)  
   
   res$df$ID <- rownames(res$df) 
   df <- merge(res$df, rowData(sim.data.nb[[k]]), by.x="ID", by.y="Gene") 
   
   list(df=df)
 })
-saveRDS(runMAST_A, ".../NBsimB/runMAST.rds")
+saveRDS(resMAST, "Simulation_study/NBsimB/resMAST.rds")
 
 
 #edgeR+Zinger
-result.edgeR_Zinger <- lapply(1:itr, function(k){
+res.edgeR_Zinger <- lapply(1:itr, function(k){
   res <- run_edgeR_Zinger(SCEdata = sim.data.nb[[k]], condition.name = "Group", 
-                                        covariates = NULL, expression.unit = "count", prop.zero = 0.99)
+                          covariates = NULL, expression.unit = "count", prop.zero = 0.99)
   res$df$ID <- rownames(res$df) 
   df <- merge(res$df, rowData(sim.data.nb[[k]]), by.x="ID", by.y="Gene") 
-
+  
   list(df=df)
 })
-saveRDS(result.edgeR_Zinger, ".../NBsimB/runZinger_EdgeR.rds")
+saveRDS(res.edgeR_Zinger, "Simulation_study/NBsimB/resZinger_EdgeR.rds")
 
 #DESeq2+Zinger
-result.DESeq2_Zinger <- lapply(1:itr, function(k){
+res.DESeq2_Zinger <- lapply(1:itr, function(k){
   res <- run_DESeq2_Zinger(SCEdata = sim.data.nb[[k]], condition.name = "Group",
                            expression.unit = "count", prop.zero = 0.99)
   res$df <- as.data.frame(res$df)
@@ -290,7 +293,7 @@ result.DESeq2_Zinger <- lapply(1:itr, function(k){
   list(df=df)
 })
 
-saveRDS(result.DESeq2_Zinger, ".../NBsimB/runZinger_DESeq2.rds")
+saveRDS(res.DESeq2_Zinger, "Simulation_study/NBsimB/resZinger_DESeq2.rds")
 
 
  
